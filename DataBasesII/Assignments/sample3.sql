516c4a20fc4b2603f2ae4aadcea8048dc98cbc62
William Carey <31861838+WilliamCareySemiColon@users.noreply.github.com>
2018-11-18 11:48:49 +0000

    Finished the functionality of the queries

--- DataBasesII/Assignments/sample3.sql
+++ DataBasesII/Assignments/sample3.sql
@@ -16,7 +16,8 @@ TO_CHAR(end_showing,'HH:MI:SS DD/MM/YYYY') "Finished Date"
 FROM DT2283GROUP_E.FILM WHERE film_rating LIKE '18only';
 
 --Aggregation to display how many seats we have in this branch of the cinemas 
-SELECT SUM(Capacity) "Total Capacity",
+SELECT COUNT(screen_no) "Total number of screens",
+SUM(Capacity) "Total Capacity",
 SUM(Prem_Capacity) "Total Premium Seats" 
 FROM DT2283GROUP_E.SCREEN; 
 
@@ -89,10 +90,11 @@ FROM DT2283GROUP_E.Ticket
 WHERE BOOKING_ID IS NULL)
 ;
 
---Inner Join 
+--Inner Join - show all the films that are currrently being showed
 SELECT film_id "Film Id", screen_no "Screen Number",
-start_time "Start Time", Capacity "Capacity", 
-prem_capacity "Prem Capacity" , release_date "Release Date",
+TO_CHAR(start_time,'HH:MI:SS DD/MM/YYYY') "Start Time",
+Capacity "Capacity", prem_capacity "Prem Capacity" ,
+TO_CHAR(release_date,'DD/MM/YYYY') "Release Date",
 film_name "Film Name", priority_rating "Film Importance",
 film_rating "Fim rating", film_duration "length",
 TO_CHAR(start_showing, 'HH:MI:SS DD/MM/YYYY') "Start",
@@ -101,21 +103,55 @@ FROM DT2283GROUP_E.SHOWING
 INNER JOIN DT2283GROUP_E.SCREEN USING(SCREEN_NO)
 INNER JOIN DT2283GROUP_E.FILM USING (FILM_ID);
 
---Outer Join
-SELECT film_id "Film Id", screen_no "Screen Number",
-start_time "Start Time", Capacity "Capacity", 
-prem_capacity "Prem Capacity" , release_date "Release Date",
-film_name "Film Name", priority_rating "Film Importance",
-film_rating "Fim rating", film_duration "length",
-TO_CHAR(start_showing, 'HH:MI:SS DD/MM/YYYY') "Start",
-TO_CHAR(end_showing, 'HH:MI:SS DD/MM/YYYY') "End"
+--Outer Join to show all the films that are either being showed or not
+--and to find what screens are been used as well
+SELECT nvl(TO_CHAR(film_id),'No Film ID') "Film Id", 
+nvl(TO_CHAR(screen_no),'No Screen Allocated') "Screen Number",
+nvl(TO_CHAR(start_time,'HH:MI:SS DD/MM/YYYY'),'Showing has no start time') "Start Time",
+nvl(TO_CHAR(Capacity),'No screen has been alloacted') "Capacity", 
+nvl(TO_CHAR(prem_capacity),'No screen has been alloacted') "Prem Capacity", 
+nvl(TO_CHAR(release_date,'DD/MM/YYYY'),'No release date') "Release Date",
+nvl(film_name,'No film has been assigned') "Film Name",
+nvl(TO_CHAR(priority_rating),'No film has been assigned') "Film Importance",
+nvl(film_rating,'No film has been assigned') "Film rating",
+nvl(TO_CHAR(film_duration),'No film has been assigned') "length", 
+nvl(TO_CHAR(start_showing, 'HH:MI:SS DD/MM/YYYY'),'No film has been assigned')
+"Start",
+nvl(TO_CHAR(end_showing, 'HH:MI:SS DD/MM/YYYY') ,'No film has been assigned')
+"End"
 FROM DT2283GROUP_E.SHOWING
 FULL OUTER JOIN DT2283GROUP_E.SCREEN USING(SCREEN_NO)
 FULL OUTER JOIN DT2283GROUP_E.FILM USING (FILM_ID);
 
---Semi-join
-SELECT * FROM DT2283GROUP_E.SHOWING
+--Semi-join to find all the showings that are connected to a ticket
+--regardless of whether it is sold or not
+SELECT showing_id "Shows Id",
+screen_no "Sceen Number",
+film_id "Film ID",
+TO_CHAR(start_time,'HH:MI:SS DD/MM/YYYY') "Start Time"
+FROM DT2283GROUP_E.SHOWING
 WHERE SHOWING_ID IN
     (SELECT showing_id FROM DT2283GROUP_E.TICKET);
---Anti-join
---Correlated sub-query.
+    
+--Anti-join to display all the films that are not being showed
+SELECT film_id "Film Id",
+TO_CHAR(release_date,'DD/MM/YYYY') "Release Date",
+film_name "Film Name",
+film_rating "Film Rating",
+film_duration "Film Duration",
+TO_CHAR(start_showing,'HH:MI:SS DD/MM/YYYY') "Starts Showing",
+TO_CHAR(end_showing,'HH:MI:SS DD/MM/YYYY') "Finishes Showing"
+FROM DT2283GROUP_E.FILM
+WHERE film_id NOT IN
+    (SELECT film_id FROM DT2283GROUP_E.SHOWING);
+    
+--Correlated sub-query to display all the films that have two 
+--or more showings recorded in the system 
+SELECT * FROM DT2283GROUP_E.FILM A
+WHERE EXISTS
+    (SELECT film_id, screen_no FROM DT2283GROUP_E.SHOWING B
+    GROUP BY screen_no, film_id
+    HAVING COUNT(screen_no) > 1
+    AND screen_no IS NOT NULL
+    AND A.film_id = B.film_id
+    );
